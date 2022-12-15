8580a9b3f60e15284c9b680442d9fde867bb0b8e
CHOQUETTEC <catherine.choquette@dfo-mpo.gc.ca>
2022-12-15 15:01:18 -0500

    Assignment 2 - practicing data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'basics-assignment-2';
+export class AppComponent implements OnInit{
+  username = '';
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+  // onTypeInput(event: Event){
+  //   this.username = (<HTMLInputElement>event.target).value;
+  // }
+
+  onButtonClick(){
+    this.username = '';
+  }
+
 }
